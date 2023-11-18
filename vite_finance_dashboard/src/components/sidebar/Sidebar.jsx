d596ffa0e1c9d7d0472e81c378e6638ab984910a
fz3hra <mujorefaatimah@gmail.com>
2023-11-18 14:06:19 +0400

    :sparkles: sidebar and nav bar

--- vite_finance_dashboard/src/components/sidebar/Sidebar.jsx
+++ vite_finance_dashboard/src/components/sidebar/Sidebar.jsx
@@ -3,18 +3,40 @@ import React from 'react'
 function Sidebar() {
   return (
     <div className='w-60 p-3 flex flex-col'>
-        <div className='flex items-center gap-2 px-1 py-3 justify-center'>
+        <div className='menu_components'>
+          <div className='flex items-center gap-2 px-1 py-3 justify-center'>
            <h1 className='text-black'>CloudFinance</h1>
         </div>
          {/* Category */}
             <div className=''>
-               <h1>MENU</h1>
+               <p className='text-sm font-medium'>MENU</p>
              </div>
             {/* Subcategory */}
-            <div className='flex flex-col items-center justify-center'>
+            <div className='flex flex-col items-start justify-center pl-2 pt-4 gap-4 text-sm text-gray-500'>
               <h2>Overview</h2>
               <h2>Statistics</h2>
+              <h2>Savings</h2>
+              <h2>Portfolios</h2>
+              <h2>Messages</h2>
+              <h2>Transactions</h2>
             </div>
+        </div>
+        <div className='general_components pt-8'>
+         {/* Category */}
+            <div className=''>
+               <p className='text-sm font-medium'>General</p>
+             </div>
+            {/* Subcategory */}
+            <div className='flex flex-col items-start justify-center pl-2 pt-4 gap-4 text-sm text-gray-500'>
+              <h2>Settings</h2>
+              <h2>Appearance</h2>
+              <h2>Need Help?</h2>
+            </div>
+        </div>
+        <div className='flex-grow'></div>
+        <div className='pl-2 text-sm font-semibold'>
+          <p>Log Out</p>
+        </div>
       </div>
   )
 }
--- vite_finance_dashboard/tailwind.config.js
+++ vite_finance_dashboard/tailwind.config.js
@@ -1,12 +1,33 @@
 /** @type {import('tailwindcss').Config} */
+const plugin = require('tailwindcss/plugin');
+
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        'placeholder-lg': '1.25rem', // Example size
+        'placeholder-sm': '0.875rem', // Example size
+      }
+    },
   },
-  plugins: [],
+  variants: {
+  },
+  plugins: [
+    plugin(function({ addUtilities, theme }) {
+      const newUtilities = {
+        '.placeholder-lg::placeholder': {
+          fontSize: theme('fontSize.placeholder-lg'),
+        },
+        '.placeholder-sm::placeholder': {
+          fontSize: theme('fontSize.placeholder-sm'),
+        },
+      };
+      addUtilities(newUtilities, ['responsive', 'hover']);
+    }),
+  ],
 }
 
